d65ef747b0f414fd80583765acd86329c66765a8
GustavoTeran <kartoffel251@hotmail.com>
2022-08-24 04:25:39 -0400

    Commit on project creation

---
+++ app/src/main/java/gus/kartoffel/kartoffel251dobcalc/MainActivity.kt
@@ -0,0 +1,11 @@
+package gus.kartoffel.kartoffel251dobcalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
